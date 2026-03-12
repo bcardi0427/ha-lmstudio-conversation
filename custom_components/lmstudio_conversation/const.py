26ab7de5043a669df9e4fc7c39125e7e1e4cf706
Gerald Haygood <github@bcardi.org>
2026-03-12 12:15:01 -0400

    Bump version to 0.5.0

--- custom_components/lmstudio_conversation/const.py
+++ custom_components/lmstudio_conversation/const.py
@@ -358,5 +358,5 @@ def option_overrides(backend_type: str) -> dict[str, Any]:
         },
     }
 
-INTEGRATION_VERSION = "0.4.9"
+INTEGRATION_VERSION = "0.5.0"
 EMBEDDED_LLAMA_CPP_PYTHON_VERSION = "0.3.16+b6153"
